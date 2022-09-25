f7588c1ef679f8b92bfde312b99685367c4c5d36
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-09-25 12:31:30 +0530

    Initial Commit

---
+++ 09-NoobCoders-iOS/_9_NoobCoders_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_NoobCoders_iOSApp.swift
+//  09-NoobCoders-iOS
+//
+//  Created by Prathamesh Araikar on 25/09/22.
+//
+
+import SwiftUI
+
+@main
+struct _9_NoobCoders_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
