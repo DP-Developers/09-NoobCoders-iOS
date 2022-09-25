f0a612a82bc101b55814161cd2b4226d790e63cc
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-26 02:42:08 +0530

    Update Color.swift

--- 09-NoobCoders-iOS/Extensions/Color.swift
+++ 09-NoobCoders-iOS/Extensions/Color.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 extension Color {
     static let theme = ColorTheme()
+    static let launch = LaunchTheme()
 }
 
 struct ColorTheme {
@@ -19,5 +20,12 @@ struct ColorTheme {
     let darkOrangeColor = Color("DarkOrangeColor")
     let lightOrangeColor = Color("LightOrangeColor")
     let secondaryText = Color("SecondaryTextColor")
+}
+
+struct LaunchTheme {
+
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
+
 }
 
