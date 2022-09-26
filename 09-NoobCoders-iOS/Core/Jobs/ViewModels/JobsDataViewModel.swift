16510faa5381db00b613f00fea8fc23f805b1d90
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-27 02:31:22 +0530

    Update JobsDataViewModel.swift

--- 09-NoobCoders-iOS/Core/Jobs/ViewModels/JobsDataViewModel.swift
+++ 09-NoobCoders-iOS/Core/Jobs/ViewModels/JobsDataViewModel.swift
@@ -5,11 +5,14 @@
 //  Created by Prathamesh Araikar on 26/09/22.
 //
 
+
 import Foundation
 import Combine
 
 class JobsDataViewModel: ObservableObject {
     @Published var allJobs: [JobsDataModel] = []
+    @Published var topJobs: [JobsDataModel] = []
+    
     let jobsDataService = JobsDataService(tag: "developer")
     private var cancellabes = Set<AnyCancellable>()
     
@@ -21,6 +24,8 @@ class JobsDataViewModel: ObservableObject {
         jobsDataService.$allJobs
             .sink { [weak self] job in
                 self?.allJobs = job
+                self?.topJobs.append(contentsOf: (self?.allJobs.prefix(3) ?? []))
+                print(self?.topJobs ?? "")
             }
             .store(in: &cancellabes)
     }
