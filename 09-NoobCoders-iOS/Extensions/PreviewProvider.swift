26120395743bd43a65acb913fc34b4e51bb1e45d
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-26 13:35:52 +0530

    developer instance for news added

--- 09-NoobCoders-iOS/Extensions/PreviewProvider.swift
+++ 09-NoobCoders-iOS/Extensions/PreviewProvider.swift
@@ -27,4 +27,13 @@ class DeveloperPreview {
         site: _9_NoobCoders_iOS.Site.codeForces,
         in24_Hours: _9_NoobCoders_iOS.In24_Hours.no,
         status: _9_NoobCoders_iOS.Status.before)
+    
+      let news = ArticleModel(source: _9_NoobCoders_iOS.Source.init(id: UUID().uuidString, name: "Google News"),
+                            author: "Prathamesh Araikar",
+                            title: "iOS 16 is here: Here's how to get Apple's free operating system update on your iPhone - The Indian Express",
+                            description: "And redesigned camera island.",
+                            url: "https://www.gsmarena.com/first_oneplus_11_pro_renders_surface_alert_slider_in_tow-news-55749.php",
+                            urlToImage: "https://images.macrumors.com/t/Rh5xQmtx7wcjjdxRsryCwyC2kng=/2126x/article-new/2022/09/apple-watch-ultra-2.jpg",
+                            publishedAt: Date(),
+                            content: "Apple may be paying the price for being too confident regarding the regular iPhone 14 models. According to analyst Ming-Chi Kuo, the iPhone 14 and 14 Plus versions are selling worse than the iPhone 1… [+3140 chars]")
 }
