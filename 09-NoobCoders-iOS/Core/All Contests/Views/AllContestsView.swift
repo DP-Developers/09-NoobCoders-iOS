908c780dd7e0490dfe4d980ee10f04248cd1b1aa
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-26 21:16:38 +0530

    In and after 24 hours headers added

--- 09-NoobCoders-iOS/Core/All Contests/Views/AllContestsView.swift
+++ 09-NoobCoders-iOS/Core/All Contests/Views/AllContestsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AllContestsView: View {
+    
     @StateObject private var vm: AllContestViewModel = AllContestViewModel()
     
     var body: some View {
@@ -16,10 +17,27 @@ struct AllContestsView: View {
             
             VStack {
                 List {
-                    ForEach(vm.allContests, content: { contest in
-                        ContestView(contest: contest)
-                    })
-                    .listRowBackground(Color(red: 0.16, green: 0.15, blue: 0.31, opacity: 1.00))
+                    Section {
+                        ForEach(vm.allContestIn24Hours, content: { contest in
+                            ContestView(contest: contest)
+                        })
+                        .listRowBackground(Color(red: 0.16, green: 0.15, blue: 0.31, opacity: 1.00))
+                    } header: {
+                        Text("IN 24 HOURS")
+                            .font(.headline)
+                            .foregroundColor(Color.theme.secondaryText)
+                    }
+                    
+                    Section {
+                        ForEach(vm.allContestAfter24Hours, content: { contest in
+                            ContestView(contest: contest)
+                        })
+                        .listRowBackground(Color(red: 0.16, green: 0.15, blue: 0.31, opacity: 1.00))
+                    } header: {
+                        Text("AFTER 24 HOURS")
+                            .font(.headline)
+                            .foregroundColor(Color.theme.secondaryText)
+                    }
                 }
                 .listStyle(.plain)
                 .clipped()
