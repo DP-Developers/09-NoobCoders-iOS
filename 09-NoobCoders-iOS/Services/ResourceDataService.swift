972c5a0c6a0dba43e713c2b56847ed82065a57e3
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-28 22:13:46 +0530

    Update ResourceDataService.swift

--- 09-NoobCoders-iOS/Services/ResourceDataService.swift
+++ 09-NoobCoders-iOS/Services/ResourceDataService.swift
@@ -10,42 +10,104 @@ import Firebase
 import FirebaseFirestore
 
 class ResourceDataService {
-    @Published var resourceService: [ResourceModel] = []
+    
+    @Published var allResources = [ResourceModel]()
+    private var db = Firestore.firestore()
+    private let currentUser = Auth.auth().currentUser?.uid ?? ""
     
     init() {
-        loadResourcesFromFirebase()
+        fetchDataFromFirebase()
     }
     
-    func loadResourcesFromFirebase() {
-        let db = Firestore.firestore()
-        
-        db.collection("resources").getDocuments { querySnapshot, error in
-            if let e = error {
-                print("There was an error in retrieving data \(e.localizedDescription)")
-            } else {
-                if let querySnapshotDocuments = querySnapshot?.documents {
-                    for document in querySnapshotDocuments {
-                        if
-                            let creator = document["creator"] as? String,
-                            let description = document["desc"] as? String,
-                            let tags = document["tags"] as? String,
-                            let title = document["title"] as? String,
-                            let type = document["type"] as? String,
-                            let url = document["url"] as? String,
-                            let upvotes = document["upvotes"] as? Int,
-                            let downvotes = document["downvotes"] as? Int {
-                            
-                            DispatchQueue.main.async {
-                                let newResource = ResourceModel(creator: creator, desc: description, tags: tags, title: title, type: type, url: url, upvotes: upvotes, downvotes: downvotes)
-                                self.resourceService.append(newResource)
-                            }
-                            
-                        } else {
-                            print("Error fetching data!!")
-                        }
+    func fetchDataFromFirebase() {
+        db.collection("resources").addSnapshotListener { querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error! No documents Found")
+                return
+            }
+            
+            self.allResources = documents.map({ (queryDocumentSnapshot) -> ResourceModel in
+                let data = queryDocumentSnapshot.data()
+                
+                let creator = data["creator"] as? String ?? ""
+                let desc = data["desc"] as? String ?? ""
+                let tags = data["tags"] as? String ?? ""
+                let title = data["title"] as? String ?? ""
+                let type = data["type"] as? String ?? ""
+                let url = data["url"] as? String ?? ""
+                let upvotes = data["upvotes"] as? Int ?? 0
+                let downvotes = data["downvotes"] as? Int ?? 0
+                let upvotedusers = data["upvotedusers"] as? [String] ?? []
+                let downvotedusers = data["downvotedusers"] as? [String] ?? []
+                
+                return ResourceModel(creator: creator, desc: desc, tags: tags, title: title, type: type, url: url, upvotes: upvotes, downvotes: downvotes, upvotedusers: upvotedusers, downvotedusers: downvotedusers)
+                
+            })
+        }
+    }
+    
+    func upvotePressed(resourceTitle: String) {
+        db.collection("resources").getDocuments { [self] querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error! No documents Found")
+                return
+            }
+            
+            for document in documents {
+                if resourceTitle == document["title"] as? String ?? "" {
+                    let resourceId = document.documentID
+                    let upvotedUsersList = document["upvotedusers"] as? [String] ?? []
+                    let downvotedusersList = document["downvotedusers"] as? [String] ?? []
+                    
+                    if (!upvotedUsersList.contains(currentUser)) && (!downvotedusersList.contains(currentUser)) {
+                        let dataRef = db.collection("resources").document(resourceId)
+                        dataRef.updateData([
+                            "upvotedusers": FieldValue.arrayUnion([currentUser] as? [String] ?? []),
+                        ])
+                    } else if (!upvotedUsersList.contains(currentUser)) && (downvotedusersList.contains(currentUser)) {
+                        let dataRef = db.collection("resources").document(resourceId)
+                        dataRef.updateData([
+                            "upvotedusers": FieldValue.arrayUnion([currentUser] as? [String] ?? []),
+                            "downvotedusers": FieldValue.arrayRemove([currentUser] as? [String] ?? [])
+                        ])
                     }
                 }
             }
         }
+        
+        self.fetchDataFromFirebase()
     }
+    
+    func downvotePressed(resourceTitle: String) {
+        db.collection("resources").getDocuments { [self] querySnapshot, error in
+            guard let documents = querySnapshot?.documents else {
+                print("Error! No documents Found")
+                return
+            }
+            
+            for document in documents {
+                if resourceTitle == document["title"] as? String ?? "" {
+                    let resourceId = document.documentID
+                    let upvotedUsersList = document["upvotedusers"] as? [String] ?? []
+                    let downvotedusersList = document["downvotedusers"] as? [String] ?? []
+                    
+                    if (!upvotedUsersList.contains(currentUser)) && (!downvotedusersList.contains(currentUser)) {
+                        let dataRef = db.collection("resources").document(resourceId)
+                        dataRef.updateData([
+                            "downvotedusers": FieldValue.arrayUnion([currentUser] as? [String] ?? [])
+                        ])
+                    } else if (upvotedUsersList.contains(currentUser)) && (!downvotedusersList.contains(currentUser)) {
+                        let dataRef = db.collection("resources").document(resourceId)
+                        dataRef.updateData([
+                            "upvotedusers": FieldValue.arrayRemove([currentUser] as? [String] ?? []),
+                            "downvotedusers": FieldValue.arrayUnion([currentUser] as? [String] ?? [])
+                        ])
+                    }
+                }
+            }
+        }
+        
+        self.fetchDataFromFirebase()
+    }
+    
 }
