98b2bbb504b3eb3c86d69dd56469e20f0556834d
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-28 22:24:50 +0530

    Update ResourceViewModel.swift

--- 09-NoobCoders-iOS/Core/Resources/ViewModels/ResourceViewModel.swift
+++ 09-NoobCoders-iOS/Core/Resources/ViewModels/ResourceViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 class ResourceViewModel: ObservableObject {
-    @Published var allResources: [ResourceModel] = []
+    @Published var allResources = [ResourceModel]()
     private let resourceDataService = ResourceDataService()
     private var cancellabes = Set<AnyCancellable>()
     
@@ -18,12 +18,56 @@ class ResourceViewModel: ObservableObject {
     }
     
     func addResources() {
-        resourceDataService.$resourceService
+        resourceDataService.$allResources
+            .map(sortResources)
             .sink { [weak self] resource in
-                DispatchQueue.main.async {
-                    self?.allResources = resource
-                }
+                self?.allResources = resource
             }
             .store(in: &cancellabes)
     }
+        
+    private func sortResources(resourceArray: [ResourceModel]) -> [ResourceModel] {
+        return resourceArray.sorted(by: { $1.upvotedusers.count < $0.upvotedusers.count })
+    }
+    
+    func filterResources(tag: String) {
+        let tags = tag.replacingOccurrences(of: ",", with: " ").lowercased()
+        self.allResources = allResources.filter { resource in
+            return resource.tags.lowercased().contains(tags) || resource.title.lowercased().contains(tags) || resource.desc.contains(tags) || resource.creator.lowercased().contains(tags)
+        }
+    }
 }
+
+
+
+
+
+// YT
+//    @Published var allResources = [ResourceModel]()
+//
+//    private var db = Firestore.firestore()
+//
+//    func fetchDataFromFirebase() {
+//        db.collection("resources").addSnapshotListener { querySnapshot, error in
+//            guard let documents = querySnapshot?.documents else {
+//                print("Error! No documents Found")
+//                return
+//            }
+//
+//            self.allResources = documents.map({ (queryDocumentSnapshot) -> ResourceModel in
+//                let data = queryDocumentSnapshot.data()
+//
+//                let creator = data["creator"] as? String ?? ""
+//                let desc = data["desc"] as? String ?? ""
+//                let tags = data["tags"] as? String ?? ""
+//                let title = data["title"] as? String ?? ""
+//                let type = data["type"] as? String ?? ""
+//                let url = data["url"] as? String ?? ""
+//                let upvotes = data["upvotes"] as? Int ?? 0
+//                let downvotes = data["downvotes"] as? Int ?? 0
+//
+//                return ResourceModel(creator: creator, desc: desc, tags: tags, title: title, type: type, url: url, upvotes: upvotes, downvotes: downvotes)
+//
+//            })
+//        }
+//    }
