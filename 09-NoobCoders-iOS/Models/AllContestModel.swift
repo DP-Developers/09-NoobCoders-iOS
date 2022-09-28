2eaf24e80cb22413b175774845a5ee737a816d17
PrathameshAraikar <114329232+PrathameshAraikar@users.noreply.github.com>
2022-09-28 22:14:48 +0530

    Update AllContestModel.swift

--- 09-NoobCoders-iOS/Models/AllContestModel.swift
+++ 09-NoobCoders-iOS/Models/AllContestModel.swift
@@ -6,13 +6,52 @@
 //
 
 import Foundation
+//
+//struct AllContestModel: Identifiable, Codable {
+//    let name: String
+//    let url: String
+//    var startTime, endTime, duration: String
+//    let site: Site
+//    var in24_Hours: In24_Hours
+//    let status: Status
+//    var id = UUID().uuidString
+//
+//    enum CodingKeys: String, CodingKey {
+//        case name, url
+//        case startTime = "start_time"
+//        case endTime = "end_time"
+//        case duration, site
+//        case in24_Hours = "in_24_hours"
+//        case status
+//    }
+//}
+//
+//enum In24_Hours: String, Codable {
+//    case no = "No"
+//    case yes = "Yes"
+//}
+//
+//enum Site: String, Codable {
+//    case atCoder = "AtCoder"
+//    case codeChef = "CodeChef"
+//    case codeForces = "CodeForces"
+//    case hackerEarth = "HackerEarth"
+//    case hackerRank = "HackerRank"
+//    case kickStart = "Kick Start"
+//    case leetCode = "LeetCode"
+//    case topCoder = "TopCoder"
+//}
+//
+//enum Status: String, Codable {
+//    case before = "BEFORE"
+//    case coding = "CODING"
+//}
 
-struct AllContestModel: Identifiable, Codable {
+struct AllContestModel:Identifiable, Codable {
     let name: String
     let url: String
-    var startTime, endTime, duration: String
-    let site: Site
-    var in24_Hours: In24_Hours
+    var startTime, endTime, duration, site: String
+    let in24_Hours: In24_Hours
     let status: Status
     var id = UUID().uuidString
 
@@ -29,17 +68,6 @@ struct AllContestModel: Identifiable, Codable {
 enum In24_Hours: String, Codable {
     case no = "No"
     case yes = "Yes"
-}
-
-enum Site: String, Codable {
-    case atCoder = "AtCoder"
-    case codeChef = "CodeChef"
-    case codeForces = "CodeForces"
-    case hackerEarth = "HackerEarth"
-    case hackerRank = "HackerRank"
-    case kickStart = "Kick Start"
-    case leetCode = "LeetCode"
-    case topCoder = "TopCoder"
 }
 
 enum Status: String, Codable {
